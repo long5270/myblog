aec109db469eefd4fd85369d7a87a251442ca655
申文龙 <shenwenlong@yimeijian.cn>
2018-12-20 18:59:05 +0800

    fix

--- controllers/default.go
+++ controllers/default.go
@@ -6,16 +6,6 @@ import (
 	"strconv"
 )
 
-type MainController struct {
-	BaseController
-}
-
-func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
-}
-
 type LoginController struct {
 	BaseController
 }
--- routers/router.go
+++ routers/router.go
@@ -6,7 +6,6 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
 	beego.Router("/login/", &controllers.LoginController{})
 	beego.Router("/register/", &controllers.RegisterController{})
 	beego.Router("/article/", &controllers.Article{}, "get:List")
